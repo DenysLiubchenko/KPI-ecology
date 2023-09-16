7c7f90760ffa241ee0c3257257a36842b4be24dd
lubchenkodenys <denyslubchenko@gmail.com>
2023-09-16 13:40:35 +0300

    added DataService

--- src/main/java/ua/kpi/kpiecologyback/controller/DataController.java
+++ src/main/java/ua/kpi/kpiecologyback/controller/DataController.java
@@ -1,9 +1,16 @@
 package ua.kpi.kpiecologyback.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ua.kpi.kpiecologyback.service.DataService;
 
 @Controller
 @RequestMapping("/data")
 public class DataController {
+    private final DataService dataService;
+    @Autowired
+    public DataController(DataService dataService) {
+        this.dataService = dataService;
+    }
 }
---
+++ src/main/java/ua/kpi/kpiecologyback/service/DataService.java
@@ -0,0 +1,23 @@
+package ua.kpi.kpiecologyback.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ua.kpi.kpiecologyback.domain.Company;
+import ua.kpi.kpiecologyback.domain.Pollutant;
+import ua.kpi.kpiecologyback.domain.Pollution;
+import ua.kpi.kpiecologyback.repository.CompanyRepository;
+import ua.kpi.kpiecologyback.repository.PollutantRepository;
+import ua.kpi.kpiecologyback.repository.PollutionRepository;
+
+@Service
+public class DataService {
+    private final CompanyRepository companyRepository;
+    private final PollutantRepository pollutantRepository;
+    private final PollutionRepository pollutionRepository;
+    @Autowired
+    public DataService(CompanyRepository companyRepository, PollutantRepository pollutantRepository, PollutionRepository pollutionRepository) {
+        this.companyRepository = companyRepository;
+        this.pollutantRepository = pollutantRepository;
+        this.pollutionRepository = pollutionRepository;
+    }
+}
