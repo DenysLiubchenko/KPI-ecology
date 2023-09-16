ec2b430322f770bfc57fc33167185b826e706911
lubchenkodenys <denyslubchenko@gmail.com>
2023-09-16 12:21:27 +0300

    initial commit

---
+++ src/main/java/ua/kpi/kpiecologyback/KpiEcologyBackApplication.java
@@ -0,0 +1,13 @@
+package ua.kpi.kpiecologyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpiEcologyBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KpiEcologyBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/kpi/kpiecologyback/KpiEcologyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kpi.kpiecologyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpiEcologyBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
