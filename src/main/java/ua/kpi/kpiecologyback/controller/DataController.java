6dfb98849c307dd50065c7914a70e39f636bc8a3
A-SyncUser <64903418+Mezozzoi@users.noreply.github.com>
2023-09-18 12:00:55 +0300

    Controller modification

--- src/main/java/ua/kpi/kpiecologyback/controller/DataController.java
+++ src/main/java/ua/kpi/kpiecologyback/controller/DataController.java
@@ -1,16 +1,23 @@
 package ua.kpi.kpiecologyback.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ua.kpi.kpiecologyback.service.DataService;
 
-@Controller
+@RestController
 @RequestMapping("/data")
 public class DataController {
     private final DataService dataService;
     @Autowired
     public DataController(DataService dataService) {
         this.dataService = dataService;
+    }
+
+    @PostMapping("/uploadCSV")
+    public void uploadCSV(@RequestBody String body) {
+        System.out.println(body);
     }
 }
