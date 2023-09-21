f53d8147a30eeaaf06f6cee5525b44a141d27f68
lubchenkodenys <denyslubchenko@gmail.com>
2023-09-21 17:10:43 +0300

    add cors to the configuration

---
+++ src/main/java/ua/kpi/kpiecologyback/configuration/MainConfiguration.java
@@ -0,0 +1,20 @@
+package ua.kpi.kpiecologyback.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MainConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+}
